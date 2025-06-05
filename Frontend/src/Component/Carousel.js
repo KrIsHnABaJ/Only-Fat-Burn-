0f3cdbace3dd64c3bb62175a856055869178c307
Krishna Bajpai <114793439+KrIsHnABaJ@users.noreply.github.com>
2025-06-05 15:14:50 +0530

    Update Carousel.js

--- Frontend/src/Component/Carousel.js
+++ Frontend/src/Component/Carousel.js
@@ -13,8 +13,6 @@ const Carousel = () => {
                               <div className="p-3" style={{ maxWidth: 900 }}>
                                   <h5 className="text-white text-uppercase">Best Gym Center</h5>
                                   <h1 className="display-2 text-white text-uppercase mb-md-4">Build Your Body Strong With Gymster</h1>
-                                  <a href className="btn btn-primary py-md-3 px-md-5 me-3">Join Us</a>
-                                  <a href className="btn btn-light py-md-3 px-md-5">Contact Us</a>
                               </div>
                           </div>
                       </div>
